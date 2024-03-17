fb7da24ef824c2722fca11c89585c727f2cd37e1
sango224 <sango.xx0224@gmail.com>
2024-03-17 21:20:02 +0900

    3/17

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var number = 3;
+var resultNum = number > 5 ? true : false;
+console.log(resultNum);
+
+var number = 3;
+var resultNum = number > 5 ? true : false;
+console.log(resultNum);
+
+var number = 8;
+var resultNum = number < 5 ? true : false;
+console.log(resultNum);
+
+var number = 5;
+var resultNum = number >= 10 ? true : false;
+console.log(resultNum);
+
+var variable;
+var result = variable !== undefined && variable !== null ? true : false;
+console.log(result);
